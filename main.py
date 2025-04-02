38d80d22d907ed62d01947c2c8c1e5043203f707
Черненко Виктория Александровна <vialchernenko@edu.hse.ru>
2025-04-02 15:11:45 +0300

    тут колбэк функций

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-from datetime import date
+
+def print_number_info(num):
+    if (num % 2) == 0:
+        print("Entered number is even")
+    else:
+        print("Entered number is odd")
 
 
-def get_weekday():
-    return date. today() .strftime('&A')
+def process_number(num, callback_fn):
+    callback_fn(num)
 
 
-def create_new_post(post, weekday=get_weekday()):
-    post_copy = post. copy()
-    post_copy[' created_on_weekday'] = weekday
-    return post_copy
-
-
-initial_post = {
-    'id': 243,
-    'author': 'Bogdan',
-}
-
-post_with_weekday = create_new_post(initial_post)
-print(post_with_weekday)
+entered_num = int(input('Enter any number:'))
+process_number(entered_num, print_number_info)
