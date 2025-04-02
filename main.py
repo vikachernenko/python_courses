fa6359793daee479741e03a7705f4bae7efd09e2
Черненко Виктория Александровна <vialchernenko@edu.hse.ru>
2025-04-02 14:57:23 +0300

    значения параметров функции по умолчанию

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-
-def merge_list_to_dict(list1, list2):
-    list1_copy = list1.copy()
-    list2_copy = list2.copy()
-    dict_from_lists = dict(zip(list1_copy, list2_copy))
-    return dict_from_lists
+from datetime import date
 
 
-list_first = ['one', 'two', 'three', 'four', 'five']
-list_second = [1, 2, 3, 4, 5]
-
-print(merge_list_to_dict(list2=list_second, list1=list_first))
-print(merge_list_to_dict(list_first, list2=list_second))
+def get_weekday():
+    return date. today() .strftime('&A')
 
 
-def update_car_info(**car):
-    car_copy = car.copy()
-    print(car_copy, type(car_copy))
-    car_copy['is_aveliable'] = True
-    return (car_copy)
+def create_new_post(post, weekday=get_weekday()):
+    post_copy = post. copy()
+    post_copy[' created_on_weekday'] = weekday
+    return post_copy
 
 
-print(update_car_info(brand='bmw', version='M5', price=499))
+initial_post = {
+    'id': 243,
+    'author': 'Bogdan',
+}
+
+post_with_weekday = create_new_post(initial_post)
+print(post_with_weekday)
