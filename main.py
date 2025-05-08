a052808e312d5d9a12e5d67b2492113d7365239a
Черненко Виктория Александровна <vialchernenko@edu.hse.ru>
2025-05-08 14:11:21 +0500

    доп символы

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import re
 
-my_string = 'my name is vika'
+my_string = 'my first name is vika.'
 
-res = re.search('v...', my_string)
+res1 = re.search('v...\.$', my_string)  # $-обозначает конец строки
+# <re.Match object; span=(17, 22), match='vika.'>
+res2 = re.search('v...', my_string)
+# <re.Match object; span=(11, 15), match='vika'>
 
-print(res)
+res3 = re.search('^m.*name', my_string)  # ^-символ начала строки
+# <re.Match object; span=(0, 7), match='my first name'>
+print(str(res1) + '\n' + str(res2) + '\n' + str(res3))
